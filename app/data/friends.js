d75439d4c9e6629e3d4725c3536e2e212c4f52e6
Trevor Wilson <trevorbwilson89@gmail.com>
2019-05-03 22:01:48 -0500

    Modal displaying closest matched friend from the data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,18 @@
 var friendsArray = [
   {
-    name: "Trevor",
+    name: "Mr. Neutral",
     photo: "https://newbuildings.org/wp-content/uploads/2015/10/mystery-man.jpg",
+    scores: [3,3,3,3,3,3,3,3,3,3]
+  },
+  {
+    name: "Dr. Disagree",
+    photo: "https://cdn-images-1.medium.com/max/2600/1*zCw9YQICYZzozYZsqeIiYA.png",
     scores: [1,1,1,1,1,1,1,1,1,1]
+  },
+  {
+    name: "Dr. Agree",
+    photo: "https://ewedit.files.wordpress.com/2017/12/gettyimages-93774328.jpg",
+    scores: [5,5,5,5,5,5,5,5,5,5]
   }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,28 @@ module.exports = function(app) {
     res.json(friendsData);
   });
   app.post("/api/friends", function(req, res) {
+    let topMatch
+    let topScore = 0
+    friendsData.forEach((item, i)=>{
+      let compScore = 0
+      if(i === 0){
+        topMatch = item
+      }
+      for(index in item.scores){
+        compScore += Math.abs(item.scores[index] - req.body.scores[index])
+      }
+      console.log(`${topScore > compScore} ${topScore} ${compScore}`)
+      if(i === 0){
+        topScore = compScore
+        topMatch = item
+      } else if (topScore > compScore){
+        topScore = compScore
+        topMatch = item
+      }
+    })
+
     friendsData.push(req.body);
-    res.json(true);
+
+    res.json(topMatch);
   });
 }
