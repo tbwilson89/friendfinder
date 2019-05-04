ce6e89ab3d3eae3689c903bd5b874f68bfd36d5d
Trevor Wilson <trevorbwilson89@gmail.com>
2019-05-03 19:43:31 -0500

    Adding survey, pushing data from survey to data file.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,9 @@ var path = require('path')
 
 module.exports = function(app) {
   app.get('/survey', (req,res)=>{
-    console.log('test')
-    res.sendFile(path.join(__dirname, '/app/public/survey.html'))
+    res.sendFile(path.join(__dirname, '../public/survey.html'))
   })
   app.get('*', (req,res)=>{
-    console.log('test')
-    res.sendFile(path.join(__dirname, '/app/public/home.html'))
+    res.sendFile(path.join(__dirname, '../public/home.html'))
   })
 }
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express')
 
-var port = process.env.PORT || 8080
+var PORT = process.env.PORT || 8080
 var app = express()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./app/routing/htmlRouts')(app)
 require('./app/routing/apiRoutes')(app)
+require('./app/routing/htmlRoutes')(app)
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
